92c810f0ba8c1c47bd1389b8931cfda2556cd5d6
AndreLobo <loboandre55@gmail.com>
2024-11-11 12:43:55 -0300

    Setting up the mapping of UserModel

---
+++ Data/Map/UserMap.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data.Map
+{
+    // Setting up the mapping of UserModel for database
+    public class UserMap : IEntityTypeConfiguration<UserModel>
+    {
+        public void Configure(EntityTypeBuilder<UserModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(256);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(158);
+        }
+    }
+}
--- obj/Debug/net8.0/SistemaDeTarefas.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaDeTarefas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb28f79d753d3c103f4fdb25d90107d5fa67da1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28445d784512f897e58fbda98e67b36f373ff58")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
