e473831823676fdb0f9d6700499567207a019b93
AndreLobo <loboandre55@gmail.com>
2024-11-12 12:37:12 -0300

    Creating a constructor for user controller and HTTP GET all users

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositories.Interfaces;
 
 namespace SistemaDeTarefas.Controllers
 {
@@ -11,10 +12,17 @@ namespace SistemaDeTarefas.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly RepoUserInterface _userRepository;
+        public UserController(RepoUserInterface userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         // Create a HTTP GET resquest for fetch all users in a List
         [HttpGet]
-        public ActionResult <List<UserModel>> GetAllUsers() {
-            return Ok();
+        public async Task<ActionResult <List<UserModel>>> GetAllUsers() {
+            List<UserModel> users = await _userRepository.FetchAllUsers();
+            return Ok(users);
         }
     }
 }
